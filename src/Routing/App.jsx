2a887b61fb5ff7722ec71ab380f979fb4cd9a41d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-13 23:39:58 +0530

    gn

--- src/Routing/App.jsx
+++ src/Routing/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {Link,BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
 
 class App extends React.Component
 {
@@ -8,17 +14,26 @@ class App extends React.Component
 
         return(
             <>
+            <Router>
                 <nav className='navbar navbar-dark bg-success navbar-expand-lg'>
-                    <a href="/home" className='navbar-brand'> WaterMelon </a>
+                    <Link to="#" className='navbar-brand'> WaterMelon </Link>
                         <div className='ml-auto'>
                             <ul className='navbar-nav'>
-                                <li className='nav-list'> <a href="" className='nav-link'> Home </a></li>
-                                <li className='nav-list'> <a href="" className='nav-link'> About </a></li>
-                                <li className='nav-list'> <a href="" className='nav-link'> Contact </a></li>
-                                <li className='nav-list'> <a href="" className='nav-link'> Services </a></li>
+                                <li className='nav-list'> <Link to="/Home" className='nav-link'> Home </Link></li>
+                                <li className='nav-list'> <Link to="/About" className='nav-link'> About </Link></li>
+                                <li className='nav-list'> <Link to="/Contact" className='nav-link'> Contact </Link></li>
+                                <li className='nav-list'> <Link to="/Services" className='nav-link'> Services </Link></li>
                             </ul>
                         </div>
                 </nav>
+
+                    <Routes>
+                        <Route path='/Home' Component={Home} />
+                        <Route path='/About' Component={About} />
+                        <Route path='/Contact' Component={Contact} />
+                        <Route path='/Services' Component={Services} />
+                    </Routes>
+            </Router>
             </>
         );
     }
