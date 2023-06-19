b36f01ebfb43b4a9fabd56c60e86e21acaeadeea
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-19 22:39:10 +0530

    gn

---
+++ src/Saga/Thunks2/react/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component
+{
+    render()
+    {
+
+        return(
+            <>
+                <h2 style={{color:"blueviolet",fontSize:"10rem",backgroundColor:"whitesmoke"}}> Thunks middleware..,</h2>
+            </>
+        );
+    }
+}
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import App from  './Thunks 1/React/App';
+import App from  './Saga/Thunks2/react/App';
 
 let container = document.getElementById('root');
 let root = ReactDOM.createRoot(container);
